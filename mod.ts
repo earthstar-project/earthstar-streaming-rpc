c02a642d67bbf4679ec25f6803e694106d3174d3
Cinnamon <cinnamon_bun_github@fastmail.com>
2022-01-20 14:09:50 -0800

    remove TransportExposedStreams

--- mod.ts
+++ mod.ts
@@ -2,6 +2,5 @@ export * from "./src/types-envelope.ts";
 export * from "./src/types.ts";
 
 export * from "./src/connection.ts";
-export * from "./src/transport-exposed-streams.ts";
 export * from "./src/transport-http-client.ts";
 export * from "./src/util.ts";
