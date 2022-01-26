b93e87d0765db8313f1ef7efab6790777c3a1220
Cinnamon <cinnamon_bun_github@fastmail.com>
2022-01-26 06:45:52 -0800

    fix npm build (example paths) and remove webStreams stuff for now

--- src/util.ts
+++ src/util.ts
@@ -38,38 +38,38 @@ export const makeDeferred = <T>(): Deferred<T> => {
     return def as Deferred<T>;
 };
 
-/**
- * Construct a ReadableStream and return it along with the controller object.
- *
- * Normally you can't access the controller object from the outside;
- * this lets you do that so you can push things into the stream more easily.
- *
- * `controller.enqueue(...)`
- */
-export interface ExposedReadableStream {
-    stream: ReadableStream;
-    controller: ReadableStreamDefaultController;
-}
-export const makeExposedStream = (
-    source: UnderlyingSource = {},
-): ExposedReadableStream => {
-    let exposedController;
-    const newSource: any = {
-        start: (controller: ReadableStreamDefaultController) => {
-            exposedController = controller;
-            if (source.start) source.start(controller);
-        },
-    };
-    if (source.pull) newSource.pull = source.pull;
-    if (source.cancel) newSource.cancel = source.cancel;
-    const stream = new ReadableStream(newSource, {
-        highWaterMark: 0,
-    });
-    return {
-        stream,
-        controller: exposedController as any as ReadableStreamDefaultController,
-    };
-};
+// /**
+//  * Construct a ReadableStream and return it along with the controller object.
+//  *
+//  * Normally you can't access the controller object from the outside;
+//  * this lets you do that so you can push things into the stream more easily.
+//  *
+//  * `controller.enqueue(...)`
+//  */
+// export interface ExposedReadableStream {
+//     stream: ReadableStream;
+//     controller: ReadableStreamDefaultController;
+// }
+// export const makeExposedStream = (
+//     source: UnderlyingSource = {},
+// ): ExposedReadableStream => {
+//     let exposedController;
+//     const newSource: any = {
+//         start: (controller: ReadableStreamDefaultController) => {
+//             exposedController = controller;
+//             if (source.start) source.start(controller);
+//         },
+//     };
+//     if (source.pull) newSource.pull = source.pull;
+//     if (source.cancel) newSource.cancel = source.cancel;
+//     const stream = new ReadableStream(newSource, {
+//         highWaterMark: 0,
+//     });
+//     return {
+//         stream,
+//         controller: exposedController as any as ReadableStreamDefaultController,
+//     };
+// };
 
 export const sleep = (ms: number): Promise<void> =>
     new Promise((resolve, reject) => setTimeout(resolve, ms));
