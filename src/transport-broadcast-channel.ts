67f9f4e69177876a9af9b83ea5cf166108482fc1
Sam Gwilym <gwilym@me.com>
2022-02-11 06:35:31 +0000

    Add some docs

--- src/transport-broadcast-channel.ts
+++ src/transport-broadcast-channel.ts
@@ -25,9 +25,13 @@ function isEnvelopeMessage<BagType extends FnsBag>(message: TransportMessage<Bag
 
 export interface ITransportBroadcastChannelOpts<BagType extends FnsBag>
     extends ITransportOpts<BagType> {
+    /** The channel for this transport's BroadcastChannel instance to join. */
     channel: string;
 }
 
+/** A Transport that connects to other transports using the same BroadcastChannel channel in other tabs or windowns on the same machine.
+ * Many TransportBroadcastChannel can join the same channel to comunicate.
+ */
 export class TransportBroadcastChannel<BagType extends FnsBag> implements ITransport<BagType> {
     status: Watchable<TransportStatus> = new Watchable('OPEN' as TransportStatus);
     deviceId: string;
@@ -42,15 +46,19 @@ export class TransportBroadcastChannel<BagType extends FnsBag> implements ITrans
         this._channel = new BroadcastChannel(opts.channel);
 
         const onEvent = (event: MessageEvent<TransportMessage<BagType>>) => {
+            // If the event contains envelope data...
             if (isEnvelopeMessage(event.data)) {
+                // Make sure we're the intnded recipient...
                 if (event.data.forDeviceId === this.deviceId) {
                     const connection = this._addOrGetConnection(event.data.envelope.fromDeviceId);
+                    // ... and handle it.
                     connection.handleIncomingEnvelope(event.data.envelope);
                 }
-
+                // This envelope wasn't for us!
                 return;
             }
 
+            // This was just another TransportBroadcastChannel telling us it exists.
             this._addOrGetConnection(event.data.deviceId);
         };
 
@@ -85,6 +93,7 @@ export class TransportBroadcastChannel<BagType extends FnsBag> implements ITrans
         log('...closed');
     }
 
+    /** Ping other transports on the same BroadcastChannel to make them create a connection to this one. */
     register() {
         log(`Registering ${this.deviceId} on "${this._channel.name}"...`);
         this._channel.postMessage({ deviceId: this.deviceId });
@@ -113,6 +122,7 @@ export class TransportBroadcastChannel<BagType extends FnsBag> implements ITrans
 
                 conn.status.set('CONNECTING');
 
+                // Send the envelope along with an ID indicating the intended recipient.
                 this._channel.postMessage({ envelope, forDeviceId: otherDeviceId });
 
                 return Promise.resolve();
